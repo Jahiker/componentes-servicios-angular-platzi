abc4554569b8ccc9fa9768e9266593b6c837af26
jahiker <rojasjahiker@gmail.com>
2022-09-10 02:15:05 -0400

    components @inputs

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
+
 export class ImgComponent implements OnInit {
+
+  @Input() img: string = 'Valor inicial'
 
   constructor() { }
 
