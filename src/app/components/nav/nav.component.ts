b308ad7afdaf32fe954a7f3a91dcdab32ed95db0
jahiker <rojasjahiker@gmail.com>
2022-09-24 00:56:50 -0400

    http cliente for api request

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
+export class NavComponent implements OnInit, OnChanges {
+  total: number = 0;
+  constructor(
+    private storeService: StoreService
+  ) {
+    this.total = this.storeService.getTotal();
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.total = this.storeService.getTotal();
   }
 
   activeMenu = false;
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from 'src/app/models/product.model';
 
+import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -9,58 +12,29 @@ import { Product } from 'src/app/models/product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   myShoppingCart: Product[] = [];
 
-  total: number = 0;
+  total = 0;
+  products: Product[] = [];
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'Album',
-      image: './assets/images/album.jpg',
-      price: 100
-    },
-    {
-      id: '2',
-      name: 'Bike',
-      image: './assets/images/bike.jpg',
-      price: 100
-    },
-    {
-      id: '3',
-      name: 'Books',
-      image: './assets/images/books.jpg',
-      price: 100
-    },
-    {
-      id: '4',
-      name: 'Glasses',
-      image: './assets/images/glasses.jpg',
-      price: 100
-    },
-    {
-      id: '5',
-      name: 'Hose',
-      image: './assets/images/house.jpg',
-      price: 100
-    },
-    {
-      id: '6',
-      name: 'Toy',
-      image: './assets/images/toy.jpg',
-      price: 100
-    }
-  ]
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService
+  ) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      console.log(data);
+      this.products = data
+    })
+  }
 
   onAddToShoppingCart(product: Product) {
-    this.myShoppingCart.push(product);
-    console.log("myShoppingCart:", this.myShoppingCart)
-    this.total =  this.myShoppingCart.reduce((sum, item) => sum + item.price, 0)
+    this.storeService.addProduct(product)
+    this.total = this.storeService.getTotal();
   }
 
 }
